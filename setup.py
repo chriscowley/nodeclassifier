75145063cc69520b5baca6166b119f9e6d7bf503
Chris Cowley <chris@chriscowley.me.uk>
2015-08-11 09:05:12 +0200

    Added a unit test on /

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 from __future__ import print_function
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import codecs
+#from setuptools.command.test import test as TestCommand
 import os
-import sys
-import re
 
 def read(fname):
+    """ function to read in a file """
+    # intentionally *not* adding an encoding option to open
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
-    name = 'nodeclassifier',
-    version = '0.1.0',
-    maintainer = 'Chris Cowley',
-    maintainer_email = 'chris@chriscowley.me.uk',
-    url = 'https://chriscowley.me.uk',
-    description = 'Helps classifier nodes for Puppet',
-    long_description = read('README.md'),
-    packages = ['nodeclassifier'],
+    name='nodeclassifier',
+    version='0.1.0',
+    maintainer='Chris Cowley',
+    maintainer_email='chris@chriscowley.me.uk',
+    url='https://chriscowley.me.uk',
+    description='Helps classifier nodes for Puppet',
+    long_description=read('README.md'),
+    packages=['nodeclassifier'],
     include_package_data=True,
-    license = 'GNU General Public License v3.0',
+    license='GNU General Public License v3.0',
     zip_safe=False,
     install_requires=find_packages(exclude=["tests/"]),
-    classifiers = [
+    classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Utilities",
     ],
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,19 @@
 """ Test module for Node Classifier """
+
+import os
+import nodeclassifier
+import unittest
+
+class NodeclassifierTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = nodeclassifier.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_root(self):
+        res = self.app.get("/")
+        assert res.status_code == 200
+
+if __name__ == '__main__':
+    unittest.main()
