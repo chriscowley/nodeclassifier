07cb78c56cd96744be2bc08ef68150ba161ade3c
Chris Cowley <chris@chriscowley.me.uk>
2015-08-16 18:51:10 +0200

    Add PEP-8 to test suite

--- test.py
+++ test.py
@@ -3,8 +3,16 @@
 import os
 import nodeclassifier
 import unittest
+import pep8
 
 class NodeclassifierTestCase(unittest.TestCase):
+    def test_pep8_conformance(self):
+        """ Test PEP8 compliance """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['nodeclassifier/views.py' ])
+        self.assertEqual(result.total_errors, 0,
+            "Found code style errors (and warnings).")
+
     def setUp(self):
         self.app = nodeclassifier.app.test_client()
 
@@ -26,6 +34,7 @@ class NodeclassifierTestCase(unittest.TestCase):
     def test_getnodes(self):
         res = self.app.get("/v1.0/nodes")
         assert res.status_code == 200
+
 
 if __name__ == '__main__':
     unittest.main()
